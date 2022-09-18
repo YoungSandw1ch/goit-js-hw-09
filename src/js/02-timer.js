b6e6727bdd0615656ae741c21f69aadf0f2082d7
Sergei Mikhailenko <sersus1991@gmail.com>
2022-09-18 15:09:07 +0300

    add some code

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,31 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
 const refs = {
   body: document.body,
+  input: document.querySelector('#datetime-picker'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
 };
 
+flatpickr('#datetime-picker', options);
+
 refs.body.style.backgroundColor = '#FFFEFE';
+refs.startBtn.addEventListener('click', onStart);
+refs.stopBtn.addEventListener('click', onStop);
+
+function onStop() {}
+function onStart() {}
