0022e75f2c3b2479f26fc8f3d48ae5f5c1eba0c6
Sergei Mikhailenko <sersus1991@gmail.com>
2022-09-18 22:41:29 +0300

    done timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
   body: document.body,
@@ -15,6 +16,8 @@ const TIMER_INTERVAL = 1000;
 let destinationTime = 0;
 let timeDifference = 0;
 let timerId = null;
+let isActive = false;
+let isPastTime = 0;
 
 const options = {
   enableTime: true,
@@ -22,43 +25,111 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    // console.log(selectedDates[0]);
-    // console.log(Date.now());
-    // console.log(selectedDates[0].getTime());
+    isPastTime = selectedDates[0].getTime() - Date.now() <= 0;
+    if (isPastTime) {
+      failureNotify();
+      return;
+    }
+
     destinationTime = selectedDates[0].getTime();
   },
 };
 
 flatpickr('#datetime-picker', options);
 
+//==========some styles for timer=================
 refs.body.style.backgroundColor = '#FFFEFE';
 refs.input.style.color = '#88f7f2';
 refs.input.style.fontFamily = 'Digital';
 refs.input.style.textAlign = 'center';
 refs.input.style.fontSize = '20px';
-
+//================================================
 refs.startBtn.addEventListener('click', onStart);
 refs.stopBtn.addEventListener('click', onStop);
 
 function onStart() {
+  if (!destinationTime || isPastTime) {
+    setNotify();
+    return;
+  }
+
+  if (isActive) {
+    infoNotify();
+    return;
+  }
+
+  isActive = true;
   timerId = setInterval(updateTime, TIMER_INTERVAL);
 }
 
-function onStop() {}
-
+function onStop() {
+  clearInterval(timerId);
+  variablesReset();
+  timerInterfaceReset();
+}
+//============callback for interval==============
 function updateTime() {
   timeDifference = destinationTime - Date.now();
+  if (timeDifference <= 0) {
+    onStop();
+    window.open('https://www.youtube.com/watch?v=-ZGlaAxB7nI');
+    bangNotify();
+    console.log('Time is over, rest in peace');
+  }
+
   secondsToTimeOnScreen(timeDifference);
 }
 
-function pad(value) {
+//============update interface====================
+function secondsToTimeOnScreen(sec) {
+  sec = Math.floor(sec / 1000);
+  refs.days.textContent = addLeadingZero(Math.floor(sec / (3600 * 24)) % 365);
+  refs.hours.textContent = addLeadingZero(Math.floor(sec / 3600) % 24);
+  refs.minutes.textContent = addLeadingZero(Math.floor(sec / 60) % 60);
+  refs.seconds.textContent = addLeadingZero(sec % 60);
+}
+
+function timerInterfaceReset() {
+  refs.days.textContent = '00';
+  refs.hours.textContent = '00';
+  refs.minutes.textContent = '00';
+  refs.seconds.textContent = '00';
+}
+
+//============support func========================
+function addLeadingZero(value) {
   return String(value).padStart(2, '0');
 }
 
-function secondsToTimeOnScreen(sec) {
-  sec = Math.floor(sec / 1000);
-  refs.days.textContent = pad(Math.floor(sec / (3600 * 24)) % 365);
-  refs.hours.textContent = pad(Math.floor(sec / 3600) % 24);
-  refs.minutes.textContent = pad(Math.floor(sec / 60) % 60);
-  refs.seconds.textContent = pad(sec % 60);
+function variablesReset() {
+  destinationTime = 0;
+  timeDifference = 0;
+  timerId = null;
+  isPastTime = 0;
+  isActive = 0;
+}
+
+//============notify==============================
+function failureNotify() {
+  Notify.failure('Please choose a date in the future', {
+    position: 'center-top',
+  });
+}
+
+function infoNotify() {
+  Notify.info('Timer already running', {
+    position: 'center-top',
+  });
+}
+
+function bangNotify() {
+  Notify.success('Time is over, rest in peace', {
+    position: 'center-top',
+  });
+}
+
+function setNotify() {
+  Notify.warning('Please select a date', {
+    position: 'center-top',
+  });
 }
