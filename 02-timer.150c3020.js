dc276c39ac38b5b11b9df534df48b8c313052113
YoungSandw1ch <sersus1991@gmail.com>
2022-09-18 07:18:15 +0000

    Deploying to gh-pages from @ YoungSandw1ch/goit-js-hw-09@a6d463e68d100dfe87e8f8bba7d3ff743588f433 🚀

---
+++ 02-timer.150c3020.js
@@ -0,0 +1,2 @@
+document.body.style.backgroundColor="#FFFEFE";
+//# sourceMappingURL=02-timer.150c3020.js.map
---
+++ 02-timer.d6a70230.js
@@ -0,0 +1,2 @@
+({body:document.body}).body.style.backgroundColor="#FFFEFE";
+//# sourceMappingURL=02-timer.d6a70230.js.map
