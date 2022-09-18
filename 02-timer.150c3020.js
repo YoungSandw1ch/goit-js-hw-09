9fae36e2f60754cc78428bba984c75b43439b225
YoungSandw1ch <sersus1991@gmail.com>
2022-09-18 12:09:37 +0000

    Deploying to gh-pages from @ YoungSandw1ch/goit-js-hw-09@b6e6727bdd0615656ae741c21f69aadf0f2082d7 🚀

--- 02-timer.150c3020.js
+++
@@ -1,2 +0,0 @@
-document.body.style.backgroundColor="#FFFEFE";
-//# sourceMappingURL=02-timer.150c3020.js.map
--- 02-timer.d6a70230.js
+++
@@ -1,2 +0,0 @@
-({body:document.body}).body.style.backgroundColor="#FFFEFE";
-//# sourceMappingURL=02-timer.d6a70230.js.map
