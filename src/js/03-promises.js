33796c24023436339506b07ad16a256820f4d102
Sergei Mikhailenko <sersus1991@gmail.com>
2022-10-01 15:20:50 +0300

    work but with shit code

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,64 +3,58 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const form = document.querySelector('.form');
 let formData = {};
 let intervaltId = null;
-let position = 0;
+let position = 1;
 
 form.addEventListener('change', onFormInputChange);
 form.addEventListener('submit', onFormSubmit);
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const shouldResolve = Math.random() > 0.3;
+    const shouldResolve = Math.random() > 0.3;
 
-      if (shouldResolve) {
-        resolve({ position, delay });
-      }
+    if (shouldResolve) {
+      resolve({ position, delay });
+    }
 
-      reject({ position, delay });
-    }, delay);
+    reject({ position, delay });
   });
-}
-
-function onFormInputChange(e) {
-  formData[e.target.name] = e.target.value;
-  console.log(formData);
 }
 
 function onFormSubmit(e) {
   e.preventDefault();
-
   let { delay, step, amount } = formData;
-  // position += 1;
 
-  createPromise(position, delay)
-    .then(({ position, delay }) => {
-      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    })
-    .catch(({ position, delay }) => {
-      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-    });
+  setTimeout(() => {
+    createPromise(position, delay).then(notifySucces).catch(notifyError);
+
+    intervaltId = setInterval(() => {
+      if (position === amount) {
+        clearInterval(intervaltId);
+        intervaltId = null;
+        position = 1;
+        return;
+      }
+
+      position += 1;
+      delay += step;
+
+      createPromise(position, delay).then(notifySucces).catch(notifyError);
+    }, step);
+  }, delay);
 }
 
-// createPromise(2, 1500)
-// .then(({ position, delay }) => {
-//   console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-// })
-// .catch(({ position, delay }) => {
-//   console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-// });
+function onFormInputChange(e) {
+  formData[e.target.name] = Number(e.target.value);
+}
 
-// intervaltId = setInterval(() => {
-//   if (position === amount) clearInterval(intervaltId);
+function notifySucces({ position, delay }) {
+  Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, {
+    timeout: 5000,
+  });
+}
 
-//   position += 1;
-//   delay += step;
-
-//   createPromise(position, delay)
-//     .then(({ position, delay }) => {
-//       console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//     })
-//     .catch(({ position, delay }) => {
-//       console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//     });
-// }, step);
+function notifyError({ position, delay }) {
+  Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, {
+    timeout: 5000,
+  });
+}
