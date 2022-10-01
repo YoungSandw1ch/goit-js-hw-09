cbb02f63bf3a015d208117d6b51c574fbb52fae1
Sergei Mikhailenko <sersus1991@gmail.com>
2022-10-01 22:58:48 +0300

    add work task with for

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,21 +2,21 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const form = document.querySelector('.form');
 let formData = {};
-let intervaltId = null;
-let position = 1;
 
 form.addEventListener('change', onFormInputChange);
 form.addEventListener('submit', onFormSubmit);
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
-    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
 
-    if (shouldResolve) {
-      resolve({ position, delay });
-    }
+      if (shouldResolve) {
+        resolve({ position, delay });
+      }
 
-    reject({ position, delay });
+      reject({ position, delay });
+    }, delay);
   });
 }
 
@@ -24,23 +24,13 @@ function onFormSubmit(e) {
   e.preventDefault();
   let { delay, step, amount } = formData;
 
-  setTimeout(() => {
-    createPromise(position, delay).then(notifySucces).catch(notifyError);
-
-    intervaltId = setInterval(() => {
-      if (position === amount) {
-        clearInterval(intervaltId);
-        intervaltId = null;
-        position = 1;
-        return;
-      }
-
-      position += 1;
+  for (let i = 1; i <= amount; i += 1) {
+    if (i !== 1) {
       delay += step;
+    }
 
-      createPromise(position, delay).then(notifySucces).catch(notifyError);
-    }, step);
-  }, delay);
+    createPromise(i, delay).then(notifySucces).catch(notifyError);
+  }
 }
 
 function onFormInputChange(e) {
@@ -58,3 +48,65 @@ function notifyError({ position, delay }) {
     timeout: 5000,
   });
 }
+
+//==================old variant==========================
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// const form = document.querySelector('.form');
+// let formData = {};
+// let intervaltId = null;
+// let position = 1;
+
+// form.addEventListener('change', onFormInputChange);
+// form.addEventListener('submit', onFormSubmit);
+
+// function createPromise(position, delay) {
+//   return new Promise((resolve, reject) => {
+//     const shouldResolve = Math.random() > 0.3;
+
+//     if (shouldResolve) {
+//       resolve({ position, delay });
+//     }
+
+//     reject({ position, delay });
+//   });
+// }
+
+// function onFormSubmit(e) {
+//   e.preventDefault();
+//   let { delay, step, amount } = formData;
+
+//   setTimeout(() => {
+//     createPromise(position, delay).then(notifySucces).catch(notifyError);
+
+//     intervaltId = setInterval(() => {
+//       if (position === amount) {
+//         clearInterval(intervaltId);
+//         intervaltId = null;
+//         position = 1;
+//         return;
+//       }
+
+//       position += 1;
+//       delay += step;
+
+//       createPromise(position, delay).then(notifySucces).catch(notifyError);
+//     }, step);
+//   }, delay);
+// }
+
+// function onFormInputChange(e) {
+//   formData[e.target.name] = Number(e.target.value);
+// }
+
+// function notifySucces({ position, delay }) {
+//   Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, {
+//     timeout: 5000,
+//   });
+// }
+
+// function notifyError({ position, delay }) {
+//   Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, {
+//     timeout: 5000,
+//   });
+// }
